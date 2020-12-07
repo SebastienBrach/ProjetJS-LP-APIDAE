1e2ea44263b138e01bda7962d7df73b5853156bf
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2020-12-07 11:32:41 +0100

    yes

---
+++ index.js
@@ -0,0 +1,82 @@
+let numAccount
+let nom
+
+class Agent {
+   
+    constructor(){
+        this.accounts = []
+        this.createAccount(1, 'Cash')
+    }
+
+    createAccount(){
+
+    }
+}
+
+class Account {
+    constructor() {
+
+    }
+}
+
+
+class Transaction {
+    constructor() {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//code de camille
+// class Agent{
+//     constructor(){
+//       this.accounts = [];
+//       this.createAccount(1, 'Cash');
+//     }
+//     ...
+//     printData(){
+//       for(let element in this.accounts){
+//         console.log(this.accounts[element].toString());
+//       }
+    
+//   }
+//   class Account{
+//     constructor(name = 'defaultValue'){
+//       this.name = name;
+//     }
+  
+//     toString(){
+//       return this.name;
+//     }
+//   }
+  
+  
+  
+//   a1 = new Agent()
+//   a1.printData()
