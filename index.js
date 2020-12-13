1fc22a63ecec735afef46f1c396f1b7b79cdd7db
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2020-12-13 18:17:16 +0100

    avancement

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ class Agent {
 	
 	// liste de compte
 	// liste de ligne de compte
-	// il s'occupe des transactions
-	
-	function bilan()
-	{
-		//parcourir les comptes, et calculer les bilans
-	}
+    // il s'occupe des transactions
+    
+    //parcourir les comptes, et calculer les bilans
+	bilan()	{
+    }
+    
+    effectuerTransaction(){
+        
+    }
+}
 
 class Account {
-    constructor(agent, numero, nom, categorie) {
-        this.agent = agent
+    constructor(numero, nom, categorie) {
         this.numero = numero
         this.nom = nom
         this.categorie = categorie
@@ -22,25 +25,37 @@ class Account {
 }
 
 class LigneDeCompte{
+    constructor(credit, debit){
+        this.credit = credit
+        this.debit = debit
+    }
 	
 }
 
-
 class Transaction {
-    constructor() {
+    constructor(compteDebite, compteCredite) {
+        this.compteDebite = compteDebite
+        this.compteCredite = compteCredite
     }
 	
 	// elle prend en paramètre le compte ou l'argent sera débité, et le compte ou l'argent sera créditée
 }
-let agent = new Agent("Sébastien")
-let account1 = new Account(agent.name, 01, "Cash","D")
-let account2 = new Account(agent.name, 02, "Emprunt","C")
-let account3 = new Account(agent.name, 03, "Capital","C")
-let account4 = new Account(agent.name, 04, "Vente","C")
-let account5 = new Account(agent.name, 05, "Achat","C")
 
-console.log(account1)
-console.log(account2)   
+// let agent = new Agent("Sébastien")
+// console.log("Bilan : "+agent.bilan)
+
+let compte1 = new Account(1, 'Cash', 'D')
+let compte2 = new Account(2, 'Emprunt', 'C')
+let compte3 = new Account(3, 'Capital', 'C')
+let compte4 = new Account(4, 'Vente', 'C')
+let compte5 = new Account(5, 'Achat', 'C')
+
+let compte = [compte1,compte2,compte3,compte4,compte5]
+
+console.log(compte)
+
+
+  
 
 
 
